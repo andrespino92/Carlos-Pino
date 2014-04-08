903721fbf015108455550a8b2f905e1731a74de5
Carlos Pino <carlos_p_11@hotmail.com>
2014-04-08 19:17:06 -0400

    first commint

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mitw::Application.config.secret_token = 'b902160484e35fc87f3fd33a1b6eb0bb797d523e29fc93689968171362ec42635fbcbe68710d8acb8ed24d201086cff5eb96e0896f54f187a82ef16ab6759fac'
